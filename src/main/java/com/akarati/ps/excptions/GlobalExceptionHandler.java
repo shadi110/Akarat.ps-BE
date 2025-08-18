eea0736df6e715fb325354e00434aee30cd12d46
Shadi Jumaa <shadijumaa92@gmail.com>
2025-08-18 20:48:15 +0300

    Fix DB connection issue

--- src/main/java/com/akarati/ps/excptions/GlobalExceptionHandler.java
+++ src/main/java/com/akarati/ps/excptions/GlobalExceptionHandler.java
@@ -9,10 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex) {
-        ErrorResponse error = new ErrorResponse(
-            HttpStatus.NOT_FOUND.value(),
-            ex.getMessage(),
-            System.currentTimeMillis());
+        ErrorResponse error = new ErrorResponse();
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
     
