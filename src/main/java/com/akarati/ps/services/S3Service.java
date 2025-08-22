ca947d2df3c78479d286d4f5ecede89cf5669276
shadi110 <shadijumaa92@gmail.com>
2025-08-22 11:42:58 +0300

    Create S3Service.java

---
+++ src/main/java/com/akarati/ps/services/S3Service.java
@@ -0,0 +1,82 @@
+package com.akarati.ps.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import java.time.Duration;
+
+import java.io.IOException;
+@Service
+public class S3Service {
+
+    private final S3Client s3;
+    private final String bucketName;
+    private final String region;
+    private final String accessKey;
+    private final String secretKey;
+
+    public S3Service(
+            @Value("${aws.accessKeyId}") String accessKey,
+            @Value("${aws.secretAccessKey}") String secretKey,
+            @Value("${aws.region}") String region,
+            @Value("${aws.s3.bucketName}") String bucketName) {
+
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+        this.bucketName = bucketName;
+        this.region = region;
+
+        this.s3 = S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)
+                ))
+                .build();
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String key = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .contentType(file.getContentType())
+                .build();
+
+        s3.putObject(putObjectRequest,
+                software.amazon.awssdk.core.sync.RequestBody.fromBytes(file.getBytes()));
+
+        return key; // Return key only; you can generate presigned URL later
+    }
+
+    public String generatePresignedUrl(String key) {
+        try (S3Presigner presigner = S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)
+                ))
+                .build()) {
+
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+
+            GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                    .signatureDuration(Duration.ofMinutes(10))
+                    .getObjectRequest(getObjectRequest)
+                    .build();
+
+            return presigner.presignGetObject(presignRequest).url().toString();
+        }
+    }
+}
