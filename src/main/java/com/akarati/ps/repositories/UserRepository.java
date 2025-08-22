76578497d41ea44d4bf70ee9f2bb700ddee85afc
shadi110 <shadijumaa92@gmail.com>
2025-08-22 11:45:25 +0300

    Update UserRepository.java

--- src/main/java/com/akarati/ps/repositories/UserRepository.java
+++ src/main/java/com/akarati/ps/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.akarati.ps.entities.User;
 
 @Repository
